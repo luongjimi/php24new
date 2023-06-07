d2f8c280edc18ac05e6856871d1680dd76734fe1
luongjimi <a1training.edu@gmail.com>
2023-06-07 18:36:43 +0700

    first commit

--- index.php
+++ index.php
@@ -17,17 +17,27 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/dist/boxicons.js"
         integrity="sha512-Dm5UxqUSgNd93XG7eseoOrScyM1BVs65GrwmavP0D0DujOA8mjiBfyj71wmI2VQZKnnZQsSWWsxDKNiQIqk8sQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css"
+        integrity="sha512-ZnR2wlLbSbr8/c9AgLg3jQPAattCUImNsae6NHYnS9KrIwRdcY9DxFotXhNAKIKbAXlRnujIqUWoXXwqyFOeIQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <div class="main">
         <div class="flx_nav">
-            <nav class="sidebar">
-                <div class="img_nav" style="margin-bottom: 15px;">
-                    <img src="img/logo2.png" alt="diblock.top">
+            <nav class="sidebar" id="side">
+                <div class="top_img_nav11">
+                    <div class="img_nav" style="margin-bottom: 15px;">
+                        <img src="img/logo2.png" alt="diblock.top">
+                        <box-icon name='x' style="height: 28px; width: 28px;" id="close_bar"
+                            onclick="closenav()"></box-icon>
+                    </div>
+                    <div
+                        style="display: flex; align-items: center; justify-content: center; margin-top: -15px; padding-bottom: 6px;">
+                        <box-icon name='chevron-down' style="width: 30px; height: 30px;"></box-icon>
+                    </div>
                 </div>
-
-                <hr>
                 <div class="menu">
                     <div class="menu_item">
                         <ul class="ul_menu">
@@ -96,11 +106,15 @@
                             <li class="item" style="margin-top: 22px;">
                                 <a href="#" class="link_item">
                                     <box-icon name='moon' class="icon21"></box-icon>
-                                    <span> Dark On </span>
+                                    <span> Dark Mode </span>
+                                    <label class="switch">
+                                        <input type="checkbox">
+                                        <span class="slider round"></span>
+                                    </label>
                                 </a>
                             </li>
                         </ul>
-                        <h6 style="text-align: center; margin-bottom: 0;"> @diblock.top -
+                        <h6 style="text-align: center;"> @diblock.top -
                             <?php echo date('Y'); ?>
                         </h6>
 
@@ -109,30 +123,74 @@
             </nav>
             <nav class="nav_top">
                 <div class="top_flx" style="justify-content: space-between;">
-                    <div class="icon31">
+
+                    <div class="icon31" onclick="opennav()">
                         <box-icon name='menu'></box-icon>
                     </div>
-                    <div class="top_flx">
 
-                        <div class="icon31">
-                            <box-icon name='bell'></box-icon>
+                    <div class="top_flx">
+                        <div class="dropdown">
+                            <div class="icon31" onclick="drFunction()" class="dropbtn">
+                                <box-icon name='bell'></box-icon>
+                            </div>
+                            <div id="myDropdown" class="dropdown-content">
+                                <h3
+                                    style="margin: 5px 5px 5px 20px; display: flex; align-items: center;justify-content: space-between; gap: 6px; white-space: nowrap;">
+                                    <p style="margin: 0;"> Thông báo </p>
+                                    <box-icon name='x' onclick="drFunction()"
+                                        style="width: 30px; height: 30px; cursor: pointer; "></box-icon>
+                                </h3>
+                                <hr>
+                                <?php for ($i=0; $i < 10 ; $i++) { 
+                                    ?> <a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sapiente
+                                    facere </a>
+                                <?php
+                                } ?>
+                            </div>
                         </div>
-                        <div class="top_flx img_us_top" style="gap: 12px;">
+                        <div class=" top_flx img_us_top" style="gap: 12px;">
                             <img src="img/anhus.jpg" alt="img_user" width="46px" height="46px"
                                 style="border-radius: 50%;">
-                            <span>Trần Đức Lương</span>
+                            <span style="white-space: nowrap;">Trần Đức Lương</span>
                         </div>
                     </div>
                 </div>
                 <hr>
                 <div class="ndung_main">
-                    <?php for ($i=0; $i <30 ; $i++) { 
-                        echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate impedit tempora debitis rem quas libero molestias cum commodi a laboriosam eveniet nostrum magni expedita rerum nisi, ratione quasi doloribus aspernatur. <br>";
-                    } ?>
+                    asd
                 </div>
             </nav>
         </div>
     </div>
+
+    <script>
+
+        function opennav() {
+            document.getElementById("side").style.display = "block";
+        };
+        function closenav() {
+            document.getElementById("side").style.display = "none";
+        };
+        function drFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        };
+
+        const body = document.body;
+        const switch_mode = document.querySelector('.slider');
+        // Load mode 
+        let mode = localStorage.getItem('darkmode');
+        if (mode == 'true') {
+            body.classList.add('dark-mode');
+
+        }
+        switch_mode.addEventListener('click', () => {
+            let mode = body.classList.toggle('dark-mode');
+            // save mode 
+            localStorage.setItem('darkmode', mode);
+        });
+
+    </script>
+
 </body>
 
 </html>
